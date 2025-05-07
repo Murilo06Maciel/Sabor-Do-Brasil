f21c3478f3ee4702af513a672dfcb656515ecd26
Murilo Maciel <murilomaciel06oficial@gmail.com>
2025-05-07 14:15:35 -0300

    Aplicação Sabor do Brasil atualizada (add imagens)

--- obj/Debug/net8.0/Sabor-Do-Brasil.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sabor-Do-Brasil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sabor-Do-Brasil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95bdc4c0ca4e7ba6559468132564ef499c8ac61d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2709480de0caa615476fa36140f2cad26fda86e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sabor-Do-Brasil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sabor-Do-Brasil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
